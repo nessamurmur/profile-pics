caa39c759ed9576de354507125100a63ce56114a
Nate West <natescott.west@gmail.com>
2015-07-23 11:59:08 -0500

    Don't worry about UUIDs

--- importer/pic_importer.py
+++ importer/pic_importer.py
@@ -8,7 +8,7 @@ default = u'http://giphy.com/gifs/cat-fail-cartoon-vdSCURsOBxZmM'
 
 
 def picture_for(user_id):
-    if  validators.uuid(user_id):
-        return datastore.fetch_user(user_id) or old_service.fetch_user(user_id) or default
-    else:
-        raise TypeError("User IDs must be UUIDs")
+    #if  validators.uuid(user_id):
+    return datastore.fetch_user(user_id) or old_service.fetch_user(user_id) or default
+    #else:
+    #    raise TypeError("User IDs must be UUIDs")
