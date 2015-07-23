f7c1108f7b865dedc0f988cfd9302dd2b2986a78
Nate West <natescott.west@gmail.com>
2015-07-23 11:57:22 -0500

    test non-uuids throw exceptions

--- test/importer_test.py
+++ test/importer_test.py
@@ -10,3 +10,9 @@ class ImporterTest(unittest.TestCase):
     @given(strategies.basic(generate=lambda _random, _: str(uuid.uuid4())))
     def test_importer_returns_an_image(self, user_id):
         assert "gif" in pic_importer.picture_for(user_id)
+
+    @given(strategies.text())
+    def test_import_with_non_uuids(self, user_id):
+        assume(not validators.uuid(user_id))
+        with self.assertRaises(TypeError):
+            pic_importer.picture_for(user_id)
